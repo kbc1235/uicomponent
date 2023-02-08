2987b8f025bfffafe67080a68727042f09fdeccd
김영우 <kyw114@mvint.co.kr>
2023-02-08 18:29:05 +0900

    ref, useEffect 사용 하여 만들기 첫번째

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,29 +19,29 @@ const Main = () => {
 
   useEffect(() => {
     let h = viewWrap.current.offsetHeight;
-    let y = h
+    let y = h;
     let max = parseInt(h * skills.length);
     setInterval(() => {
-        if (h >= max) {
-          h = 0;
-          dom.current.style.transform = `translateY(${h}px)`;
-        }
-        dom.current.style.transform = `translateY(-${h}px)`;
-        h += y;
-      }, 2000);
+      if (h >= max) {
+        h = 0;
+        dom.current.style.transform = `translateY(${h}px)`;
+      }
+      dom.current.style.transform = `translateY(-${h}px)`;
+      h += y;
+    }, 2000);
   });
 
   return (
-    <div className="">
-      <div>
+    <div className={styles.main_wrap}>
+      <div className={styles.main}>
+        <div className={styles.main_title}>I can use</div>
         <div className={styles.skill_box} ref={viewWrap}>
           <div className={styles.skill_view} ref={dom}>
             {skills.map((data) => {
-              return <Skill skill={data} key={data}/>;
+              return <Skill skill={data} key={data} />;
             })}
           </div>
         </div>
-        <div></div>
       </div>
     </div>
   );
