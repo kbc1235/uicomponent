fb40a8240c5bb6f8a15c22459db4d9f94d1174b8
김영우 <kyw114@mvint.co.kr>
2023-02-14 17:39:55 +0900

    carousel 완성

--- src/components/CarouselWrap.jsx
+++ src/components/CarouselWrap.jsx
@@ -1,37 +1,61 @@
-import { useEffect, useRef } from 'react';
-import styles from '../style/style.module.css';
-import CarouselItem from './CarouselItem';
-import Forwardedrefcarousel from './Carousel';
+import { useEffect, useRef, useState } from "react";
+import styles from "../style/style.module.css";
+import CarouselItem from "./CarouselItem";
+import Forwardedrefcarousel from "./Carousel";
 
-const CarouselWrap = () =>{
-  const carousel = useRef();
-  useEffect(()=>{
-    const $carousel =  carousel.current
-    let w = $carousel.children.length;
+const TOTAL_SLIDES = 2;
+const CarouselWrap = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const carouselRef = useRef(null);
 
-    $carousel.style.width = `calc(100% * ${w})`
+  const NextSlide = () => {
+    if (currentSlide >= TOTAL_SLIDES) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+  // Prev 버튼 클릭 시
+  const PrevSlide = () => {
+    if (currentSlide === 0) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
 
+  useEffect(() => {
+    carouselRef.current.style.transition = "all 0.5s ease-in-out";
+    carouselRef.current.style.transform = `translateX(-${currentSlide}00%)`;
+  }, [currentSlide]);
 
-    
-  })
-  
-
-  return(
+  return (
     <div className={styles.carouselwrap}>
-      <Forwardedrefcarousel ref={carousel}>
-          <CarouselItem imgName={'https://placeholder.com/assets/images/150x150-2-500x500.png'}/>
-          <CarouselItem imgName={'https://placeholder.com/assets/images/150x150-2-500x500.png'}/>
-          <CarouselItem imgName={'https://placeholder.com/assets/images/150x150-2-500x500.png'}/>
-          <CarouselItem imgName={'https://placeholder.com/assets/images/150x150-2-500x500.png'}/>
-          <CarouselItem imgName={'https://placeholder.com/assets/images/150x150-2-500x500.png'}/>
-          <CarouselItem imgName={'https://placeholder.com/assets/images/150x150-2-500x500.png'}/>
-          <CarouselItem imgName={'https://placeholder.com/assets/images/150x150-2-500x500.png'}/>
-          <CarouselItem imgName={'https://placeholder.com/assets/images/150x150-2-500x500.png'}/>
+      <Forwardedrefcarousel ref={carouselRef}>
+        <CarouselItem
+          imgName={
+            "https://placeholder.com/assets/images/150x150-2-500x500.png"
+          }
+        />
+        <CarouselItem
+          imgName={
+            "https://placeholder.com/assets/images/150x150-2-500x500.png"
+          }
+        />
+        <CarouselItem
+          imgName={
+            "https://placeholder.com/assets/images/150x150-2-500x500.png"
+          }
+        />
       </Forwardedrefcarousel>
-      <button type='button'>이전</button>
-      <button type='button'>다음</button>
+      <button type="button" onClick={PrevSlide}>
+        이전
+      </button>
+      <button type="button" onClick={NextSlide}>
+        다음
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default CarouselWrap;
